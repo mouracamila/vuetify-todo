432b2461d7ba06a238a085b74a00746ae097b680
Camila Moura <camilapaleo@gmail.com>
2019-05-05 15:27:22 +0200

    first commit

---
+++ src/views/Projects.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="projects">
+    <h1>Projects</h1>
+
+  
+  </div>
+    
+
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
---
+++ src/views/Team.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="teams">
+    <h1>Team</h1>
+
+  
+  </div>
+    
+
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
